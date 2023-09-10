2b2af43c2e199e81b6d218e33bf202381b9b47cb
tspiyush <piyushmakwana@outlook.in>
2023-09-10 08:15:04 +0530

    1.7.8_ALPHA08

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.7.8_ALPHA08"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
